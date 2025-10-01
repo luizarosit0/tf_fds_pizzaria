fc881eb4d6d876c67718fce8ef234fa97c46e710
Luiza Ilha Rosito <luiza.rosito@edu.pucrs.br>
2025-10-01 21:50:17 +0000

    inicio

---
+++ pizzaria/src/main/java/com/luiza/ex4_lancheriaddd_v1/Aplicacao/Responses/CabecalhoCardapioResponse.java
@@ -0,0 +1,8 @@
+package com.luiza.ex4_lancheriaddd_v1.Aplicacao.Responses;
+
+import java.util.List;
+
+import com.luiza.ex4_lancheriaddd_v1.Dominio.Entidades.CabecalhoCardapio;
+
+public record CabecalhoCardapioResponse(List<CabecalhoCardapio> cabecalhos) {
+}
