347786619f69984d8096570c7704bca33f40ddaf
Julia Fernandes <f.julia001@edu.pucrs.br>
2025-10-06 13:19:23 +0000

    resolvezndo os conflitos restantes do com.bcopstein

--- pizzaria/src/main/java/com/luiza/ex4_lancheriaddd_v1/Ex4LancheriadddV1Application.java
+++ pizzaria/src/main/java/com/luiza/ex4_lancheriaddd_v1/Ex4LancheriadddV1Application.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.bcopstein")
+@ComponentScan("com.luiza")
 public class Ex4LancheriadddV1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ex4LancheriadddV1Application.class, args);
-	}
-
+		public static void main(String[] args) {
+				SpringApplication.run(Ex4LancheriadddV1Application.class, args);
+					}
+					
 }
