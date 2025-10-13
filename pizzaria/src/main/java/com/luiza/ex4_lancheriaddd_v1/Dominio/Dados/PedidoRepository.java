8a7be67fd661a41b3bc084d94f42b63fa1235e54
Julia Fernandes <f.julia001@edu.pucrs.br>
2025-10-13 19:03:27 +0000

    solução do problema do return em PedidoService

--- pizzaria/src/main/java/com/luiza/ex4_lancheriaddd_v1/Dominio/Dados/PedidoRepository.java
+++ pizzaria/src/main/java/com/luiza/ex4_lancheriaddd_v1/Dominio/Dados/PedidoRepository.java
@@ -3,7 +3,7 @@ package com.luiza.ex4_lancheriaddd_v1.Dominio.Dados;
 import com.luiza.ex4_lancheriaddd_v1.Dominio.Entidades.Pedido;
 
 public interface PedidoRepository {
-    void salvar(Pedido pedido);
+    Pedido salvar(Pedido pedido);
     Pedido buscarPorId(long id);
     // metodo para o DescontoService
     int quantPedidos(String CPF, int dias);
