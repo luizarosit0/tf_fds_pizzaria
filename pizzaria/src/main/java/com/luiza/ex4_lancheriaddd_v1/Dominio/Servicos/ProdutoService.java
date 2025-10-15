3649699843ccc86444a2abc693b4269c34b0400f
Julia Fernandes <f.julia001@edu.pucrs.br>
2025-10-15 18:24:32 +0000

    commit

--- pizzaria/src/main/java/com/luiza/ex4_lancheriaddd_v1/Dominio/Servicos/ProdutoService.java
+++ pizzaria/src/main/java/com/luiza/ex4_lancheriaddd_v1/Dominio/Servicos/ProdutoService.java
@@ -5,23 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.luiza.ex4_lancheriaddd_v1.Dominio.Dados.CardapioRepository;
+import com.luiza.ex4_lancheriaddd_v1.Dominio.Dados.ProdutosRepository;
 import com.luiza.ex4_lancheriaddd_v1.Dominio.Entidades.Produto;
 
 @Service
 public class ProdutoService {
-    private ProdutoRepository produtoRepository;
+    private ProdutosRepository produtosRepository;
 
     @Autowired
-    public ProdutoService(ProdutoRepository produtoRepository){
-        this.produtoRepository = produtoRepository;
+    public ProdutoService(ProdutosRepository produtosRepository){
+        this.produtosRepository = produtosRepository;
     }
 
-    public Produto recuperaProdutoPorid(long id);{
-        return produtoRepository.recuperaProdutoPorid(id);
+    public Produto recuperaProdutoPorid(long id){
+        return produtosRepository.recuperaProdutoPorid(id);
     }
 
-    public List<Produto> recuperaProdutosCardapio(long id);{
-        return produtoRepository.recuperaProdutosCardapio(id);
-    }
+    public List<Produto> recuperaProdutosCardapio(long id){
+        return produtosRepository.recuperaProdutosCardapio(id);
+    } 
 }
